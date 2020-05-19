fbe4ffae357d5aa984d80dd996dfbb1a931f8f25
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-05-19 16:59:58 +0100

    Configured App Entry Point and Routing setup working fine

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+//We'll use webpack to bundle css
+import App from "./Components/App.js";
+import "./index.css";
 
-function Hi() {
-  return <p> Hi</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
