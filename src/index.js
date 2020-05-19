8c2b3db36fcf50cc269af7fe0e3ea6a3fe98b489
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-05-19 11:03:50 +0100

    ESLint Config set up

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 function Hi() {
-  debugger;
   return <p> Hi</p>;
 }
-
-//Create the function and then create then render below and point it to App
 
 render(<Hi />, document.getElementById("app"));
