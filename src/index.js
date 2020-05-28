4c1a54e6700324f5ca410e586bf3af1ab5fdb1bb
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-05-28 16:53:00 +0100

    Instantiated Store and wrapped the App in a Provider so I can use the Store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-//We'll use webpack to bundle css
 import App from "./Components/App.js";
 import "./index.css";
+import configureStore from "./Redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
+const store = configureStore();
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
