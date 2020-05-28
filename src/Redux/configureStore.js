e89384e70c831132f59bff90942b61a2d3a4df3e
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-05-28 16:40:49 +0100

    Created Store, applied middleware and configured Redux Dev Tools

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -5,12 +5,9 @@ import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 export default function configureStore(initalState) {
   const composeEnchancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  // This will add support for Redux Dev Tools. This avoids name collision
   return createStore(
     rootReducer,
     initalState,
     composeEnchancers(applyMiddleware(reduxImmutableStateInvariant()))
   );
 }
-
-// componseEnchancers calls applyMiddleware and reduxImmutableStateInvariant is a piece of middleware we're using. Now we'll be able to interact with our Redux store using the Redux Dev tools in the browser
