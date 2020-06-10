6e842c2dcdc5c93d6102c92ce0d4f1a214c96d92
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-06-10 11:50:17 +0100

    Created State for Form Data using useState/React Hooks

--- src/Components/Courses/ManageCoursePage.js
+++ src/Components/Courses/ManageCoursePage.js
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../Redux/Actions/courseActions";
 import { loadAuthors } from "../../Redux/Actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+
   useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
@@ -27,6 +37,7 @@ function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
 }
 
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
@@ -35,6 +46,7 @@ ManageCoursePage.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors,
   };
