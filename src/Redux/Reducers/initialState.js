2491c10a71e58b5caf8139a9f69cd717a9494611
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-06-16 17:04:18 +0100

    Created API Status Actions and added the reducer to root reducer

---
+++ src/Redux/Reducers/initialState.js
@@ -0,0 +1,6 @@
+export default {
+  courses: [],
+  authors: [],
+  apiCallsInProgress: 0,
+  // We're setting this at 0 as we want to track the number of API calls in progress as we could havve multiple calls going at any given time
+};
