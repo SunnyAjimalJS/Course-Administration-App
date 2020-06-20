a2d9ce857ea506291c5cd223a82a69ab9f9226d3
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-06-20 17:14:19 +0100

    Tested the label on the Save button using react-testing-library. First tested that it should say Save when it's not saving, then tested it say Saving... when the saving prop is set to true

--- src/Components/Courses/CourseForm.ReactTestingLibrary.test.js
+++ src/Components/Courses/CourseForm.ReactTestingLibrary.test.js
@@ -22,3 +22,13 @@ it("should render Add Course header", () => {
   const { getByText } = renderCourseForm();
   getByText("Add Course");
 });
+
+it('should label save button as "Save" when not saving', () => {
+  const { getByText } = renderCourseForm();
+  getByText("Save");
+});
+
+it('should label save button as "Saving" when saving', () => {
+  const { getByText } = renderCourseForm({ saving: true });
+  getByText("Saving...");
+});
